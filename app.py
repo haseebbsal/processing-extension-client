d52ed6ecce5df7fdfe5651918dfb1a532ed81639
shumman <shumman2007@hotmail.com>
2024-04-08 23:13:24 +0500

    adding files

--- app.py
+++ app.py
@@ -26,11 +26,12 @@ def uploadManu():
     s3 = boto3.client('s3',
                       aws_access_key_id=aws_access,
                       aws_secret_access_key=aws_secret)
-    new_df=list(pd.read_csv('https://markjbs.s3.us-west-2.amazonaws.com/Brand-supplier.csv',encoding='iso-8859-1',header=None)[0])
-    brand_lower_set = set(new_df)
-    brand_new_lower_set = set(pd.read_csv('https://markjbs.s3.us-west-2.amazonaws.com/Brand-supplier1.csv',encoding='iso-8859-1',header=None)[0])
-    brand_lower_set=brand_lower_set.union(brand_new_lower_set)
-    brand_dataframe=pd.DataFrame(list(brand_lower_set))
+    new_df=pd.read_csv('https://markjbs.s3.us-west-2.amazonaws.com/Brand-supplier.csv',encoding='iso-8859-1',header=None,dtype='object').values.tolist()
+    brand_new_lower_set = pd.read_csv('https://markjbs.s3.us-west-2.amazonaws.com/Brand-supplier1.csv',encoding='iso-8859-1',header=None,dtype='object').values.tolist()
+    for i in brand_new_lower_set:
+        new_df.append(i)
+    # brand_lower_set=brand_lower_set.union(brand_new_lower_set)
+    brand_dataframe=pd.DataFrame(new_df)
     csv_buffer=BytesIO()
     brand_dataframe.to_csv(csv_buffer,index=False)
     csv_buffer.seek(0)
